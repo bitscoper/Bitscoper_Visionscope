244b9fb915476f966ea7e8819cf7a22ddea65727
Abdullah As-Sadeed <bitscoper@tutanota.com>
2026-03-07 05:45:01 +0600

    Moved Model Path Resolution to `main.py`; Declared Types of Returned Variables from OpenCV Capture; Sorted Linted Imports; Updated an Error String; Added Line Length in `pyproject.yaml` for Black Formatter; Added Docstrings for Modules; and Formatted Files.

--- settings.py
+++ settings.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""By Abdullah As-Sadeed"""
 
-# By Abdullah As-Sadeed
+MODEL_DIRECTORY = "Ultralytics_YOLO_Weights"
 
-import sys
-from pathlib import Path
-
-file_path = Path(__file__).resolve()
-root_path = file_path.parent
-
-if root_path not in sys.path:
-    sys.path.append(str(root_path))
-
-ROOT = root_path.relative_to(Path.cwd())
-
-MODEL_DIRECTORY = ROOT / "Ultralytics_YOLO_Weights"
+DEFAULT_CONFIDENCE = 0.40
 
 DEFAULT_WEBCAM_NUMBER = 0
 DEFAULT_VIDEO_WIDTH = 640
 DEFAULT_VIDEO_HEIGHT = 480
-
-DEFAULT_CONFIDENCE = 0.40
